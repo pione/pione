438e5616149beae4801b8491ca2d474e92d3e6e7
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2012-05-24 20:46:21 +0900

    Commit current work.

--- test/spec_update-criteria.rb
+++ test/spec_update-criteria.rb
@@ -2,6 +2,22 @@ require 'innocent-white/test-util'
 
 data = DataExpr.new('*')
 
+document = Document.parse(<<DOCUMENT)
+Rule NoOutputsRule
+  input '*'
+Action---
+
+--End
+
+Rule OutputsRule
+  inputs '*.a'
+  output '*'.except('{$INPUT[1]}')
+Action---
+echo -n "abc" > {$OUTPUT[1]}
+---End
+DOCUMENT
+
+
 time1 = Time.now
 time2 = Time.now
 time3 = Time.now
@@ -12,39 +28,41 @@ tuple3 = Tuple[:data].new('test', '3', nil, time2)
 UC = UpdateCriteria
 
 describe 'UpdateCriteria' do
+
+  # no output rules
   describe 'criteria: no output rules' do
     it 'should be updatable' do
-      inputs = [data]
-      outputs = []
-      rule = Rule::BaseRule.new('test', inputs, outputs, [], [], nil)
+      inputs = [tuple1, tuple2]
+      outputs = [tuple3]
 
-      input_tuples = [tuple1, tuple2]
-      output_tuples = [tuple3]
-
-      UC.no_output_rules?(rule, input_tuples, output_tuples).should.true
+      UC.no_output_rules?($no_outputs_rule, inputs, outputs).should.true
     end
 
     it 'should not be updatable' do
-      inputs = [data]
-      outputs = [data]
-      rule = Rule::BaseRule.new('test', inputs, outputs, [], [], nil)
-
       input_tuples = [tuple1, tuple2]
       output_tuples = [tuple3]
 
-      UC.no_output_rules?(rule, input_tuples, output_tuples).should.false
+      UC.no_output_rules?($outputs_rule, inputs, outputs).should.false
     end
   end
 
   describe 'criteria: not_exist_output' do
+    before do
+      inputs = [data]
+      outputs = [data]
+      @rule = Rule::BaseRule.new('test', inputs, outputs, [], [], nil)
+    end
+
     it 'should be updatable' do
       inputs = [tuple1, tuple2]
-      outputs = [tuple3]
-      UC.no_output_rules?(@rule, inputs, outputs).should.true
+      outputs = []
+      UC.not_exist_output?(@rule, inputs, outputs).should.true
     end
 
     it 'should be not updatable' do
-      
+      inputs = []
+      outputs = []
+      UC.not_exist_output?(@rule)
     end
 
   end
