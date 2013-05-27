b8b4034e86ab4e5af281e5fe7028ecaa0749eda3
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2013-05-27 16:31:23 +0900

    Added some options for endurance test.

--- test/endurance-test/run.sh
+++ test/endurance-test/run.sh
@@ -1,13 +1,50 @@
 #!/bin/sh
 
 N=100
+LOCATION=example/HelloWorld/
+CLEAR=1
+LIMIT=10
 
-if [ "$1" != "" ]
-then
-    N=$1
-fi
+usage() {
+    echo "Usage: run.sh [-n NUMBER] [-l LOCATION] [-z] [--limit SEC]"
+    echo "         -n NUMBER     do the test NUMBER times"
+    echo "         -l LOCATION   location of process document"
+    echo "         -z            don't clear output location"
+    echo "         --limit SEC   timeout after SEC seconds"
+    exit 1
+}
 
-echo "test $N times"
+OPT=`getopt -o n:l:z -l limit: -- "$@"`; [ $? -ne 0 ] && usage
+eval set -- "$OPT"
+
+while true
+do
+    case $1 in
+	-n)
+	    N=$2; shift 2
+	    ;;
+	-l)
+	    LOCATION="$2"; shift 2
+	    ;;
+	-z)
+	    CLEAR=0; shift
+	    ;;
+	--limit)
+	    LIMIT=$2; shift 2
+	    ;;
+	--)
+	    shift; break
+	    ;;
+	*)
+	    usage
+	    ;;
+    esac
+done
+
+echo "endurance-test($N times)"
+echo "  location: $LOCATION"
+echo "  limit: $LIMIT"
+echo "  clear output: $CLEAR (1: true, 0: faluse)"
 RESULT=endurance-test-result.txt
 TIME=endurance-test-time.txt
 
@@ -17,7 +54,11 @@ echo "number, real, user, sys" > ${TIME}
 for i in `seq 1 $N`
 do
     echo "--- ${i} ---"
-    timeout -s 9 10 time --quiet --append -o ${TIME} -f "${i}, %e, %U, %S"  pione-client example/HelloWorld/ --rehearse
+    if [ $CLEAR -eq 1 ]
+    then
+	rm -rf output
+    fi
+    timeout -s 9 $LIMIT time --quiet --append -o ${TIME} -f "${i}, %e, %U, %S"  pione-client ${LOCATION} --rehearse
     echo "${i}, $?" >> ${RESULT}
     sleep 1
     pkill -KILL -fe pione-
