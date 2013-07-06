e1bf42b4e6eaada798a067bd61c7310e6781b6b1
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2013-07-06 18:30:21 +0900

    Refactored test utility for tranformers.

--- test/test-util/transformer.rb
+++ test/test-util/transformer.rb
@@ -1,53 +1,41 @@
 module TestUtil
   module Transformer
+    # test case
     class TestCase < StructX
       member :string
       member :expected
     end
 
-    class TestCaseEq < StructX
-      member :string
-      member :expected
+    # test case list
+    class TestCases < Array
+      def test(obj, res=nil, &b)
+        if res
+          push(Naming.Eq(TestCase.new(obj, res)))
+        else
+          push(Naming.Block(TestCase.new(obj, b)))
+        end
+      end
     end
 
+    # Define transformer specification.
     def spec(name, parser, context, &b)
-      testcases = Array.new
+      testcases = TestCases.new.tap {|x| x.instance_eval(&b)}
 
-      def testcases.tc(obj)
-        case obj
-        when Hash
-          obj.each do |key, val|
-            push(TestCaseEq.new(key, val))
-          end
-        else
-          push(TestCaseEq.new(obj, yield))
-        end
-      end
-
-      def testcases.transform(obj, &b)
-        push(TestCase.new(obj, b))
-      end
-
-      def testcases.test(obj, res=nil, &b)
-        if res
-          push(TestCaseEq.new(obj, res))
-        else
-          push(TestCase.new(obj, b))
-        end
-      end
-
-      testcases.instance_eval(&b)
+      # setup bacon context
       context.describe name do
         testcases.each do |tc|
-          it "should get %s:%s%s" % [name, tc.string.include?("\n") ? "\n" : " ", tc.string.chomp] do
+          string = tc.value.string
+          expected = tc.value.expected
+
+          it "should get %s:%s%s" % [name, string.include?("\n") ? "\n" : " ", string.chomp] do
             res = DocumentTransformer.new.apply(
-              DocumentParser.new.send(parser).parse(tc.string)
+              DocumentParser.new.send(parser).parse(string)
             )
             case tc
-            when TestCaseEq
-              res.should == tc.expected
-            when TestCase
-              tc.expected.call(res)
+            when Naming::Eq
+              res.should == expected
+            when Naming::Block
+              expected.call(res)
             end
           end
         end
