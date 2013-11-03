970d18fcb0f99b776cc2a87d769227ec9730c8b2
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2013-11-03 22:24:53 +0900

    Update Sum package.

--- test/command/spec_pione-client.rb
+++ test/command/spec_pione-client.rb
@@ -242,5 +242,15 @@ TestHelper.scope do |this|
       end
     end
 
+    describe "example/Sum" do
+      TestHelper::PioneClientRunner.test(self) do |runner|
+        runner.title = "should get a result of example/Sum"
+        runner.args = ["example/Sum", "--rehearse", *runner.default_arguments]
+        runner.run do |base|
+          (base + "sum.txt").should.exist
+        end
+      end
+    end
+
   end
 end
