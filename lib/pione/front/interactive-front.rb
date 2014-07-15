645968ea0bfd3b01606d5e39f24ede8649fa75e5
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2014-07-15 22:02:18 +0900

    Commit current work.

--- lib/pione/front/interactive-front.rb
+++ lib/pione/front/interactive-front.rb
@@ -11,8 +11,50 @@ module Pione
       #
       # @param [String] path
       #   relative path from public directory
-      def file(path)
+      def get(path)
         (@cmd.model[:public] + path).read
+      end
+
+      # Create a file with the content. Thie operation returns true only if the
+      # file creation has succeeded.
+      def create(path, content)
+        begin
+          (@cmd.model[:public] + path).write(content)
+          return true
+        rescue => e
+          return false
+        end
+      end
+
+      # Delete the file. Thie operation returns true only if the file deletion
+      # has succeeded.
+      def delete(path)
+        begin
+          (@cmd.model[:public] + path).delete
+          return true
+        rescue => e
+          return false
+        end
+      end
+
+      # Return entry informations in the directory. When the operation returns
+      # nil, the file listing has failed. When this returns false, the path is
+      # file.
+      def list(path)
+        begin
+          unless (@cmd.model[:public] + path).directory?
+            return false
+          end
+
+          (@cmd.model[:public] + path).entries.map do |entry|
+            { "name"  => entry.basename,
+              "type"  => entry.directory? ? "dir" : "file",
+              "mtime" => entry.mtime.iso8601,
+              "size"  => entry.size }
+          end
+        rescue => e
+          return nil
+        end
       end
     end
   end
