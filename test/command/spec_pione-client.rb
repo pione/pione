0ec65c3af2ce7caeb41758ef77343a7d448536c5
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2015-01-03 02:23:54 +0900

    Fixed test bug for pione-client.

--- test/command/spec_pione-client.rb
+++ test/command/spec_pione-client.rb
@@ -15,32 +15,32 @@ TestHelper.scope do |this|
       path = Temppath.create
       cmd = @cmd.new(["example/HelloWorld/HelloWorld.pione", "--base", path.to_s])
       res = Rootage::ScenarioTest.succeed(cmd)
-      Location[path + "message.txt"].should.exist
-      Location[path + "message.txt"].read.should.start_with "Hello, world!"
+      Location[path + "output" + "message.txt"].should.exist
+      Location[path + "output" + "message.txt"].read.should.start_with "Hello, world!"
     end
 
     it "should execute a PIONE document with stand alone mode" do
       path = Temppath.create
       cmd = @cmd.new(["example/HelloWorld/HelloWorld.pione", "--base", path.to_s, "--stand-alone"])
       Rootage::ScenarioTest.succeed(cmd)
-      Location[path + "message.txt"].should.exist
-      Location[path + "message.txt"].read.should.start_with "Hello, world!"
+      Location[path + "output" + "message.txt"].should.exist
+      Location[path + "output" + "message.txt"].read.should.start_with "Hello, world!"
     end
 
     it "should execute a PIONE package" do
       path = Temppath.create
-      cmd = @cmd.new(["example/HelloWorld/", "-o", path.to_s])
+      cmd = @cmd.new(["example/HelloWorld/", "--base", path.to_s])
       Rootage::ScenarioTest.succeed(cmd)
-      Location[path + "message.txt"].should.exist
-      Location[path + "message.txt"].read.should.start_with "Hello, world!"
+      Location[path + "output" + "message.txt"].should.exist
+      Location[path + "output" + "message.txt"].read.should.start_with "Hello, world!"
     end
 
     it "should execute a PIONE package with stand alone mode" do
       path = Temppath.create
       cmd = @cmd.new(["example/HelloWorld/", "--base", path.to_s, "--stand-alone"])
       Rootage::ScenarioTest.succeed(cmd)
-      Location[path + "message.txt"].should.exist
-      Location[path + "message.txt"].read.should.start_with "Hello, world!"
+      Location[path + "output" + "message.txt"].should.exist
+      Location[path + "output" + "message.txt"].read.should.start_with "Hello, world!"
     end
 
     it "should fail with action error" do
@@ -55,8 +55,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result with no parameters (default fib(3))"
         runner.args = ["example/Fib/Fib.pione", *runner.default_arguments]
         runner.run do |base|
-          (base + "result.txt").should.exist
-          (base + "result.txt").read.should.start_with "2"
+          (base + "output" + "result.txt").should.exist
+          (base + "output" + "result.txt").read.should.start_with "2"
         end
       end
 
@@ -78,8 +78,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result of example/PegasusWMS/Merge"
         runner.args = ["example/PegasusWMS/Merge/", *runner.default_arguments]
         runner.run do |base|
-          (base + "binaries.txt").should.exist
-          (base + "binaries.txt").size.should > 0
+          (base + "output" + "binaries.txt").should.exist
+          (base + "output" + "binaries.txt").size.should > 0
         end
       end
     end
@@ -90,8 +90,8 @@ TestHelper.scope do |this|
           runner.title = "should get a result of example/PegasusWMS/Pipeline"
           runner.args = ["example/PegasusWMS/Pipeline/", *runner.default_arguments]
           runner.run do |base|
-            (base + "count.txt").should.exist
-            (base + "count.txt").size.should > 0
+            (base + "output" + "count.txt").should.exist
+            (base + "output" + "count.txt").size.should > 0
           end
         end
       else
@@ -105,14 +105,14 @@ TestHelper.scope do |this|
           runner.title = "should get a result of example/PegasusWMS/Split"
           runner.args = ["example/PegasusWMS/Split/", *runner.default_arguments]
           runner.run do |base|
-            (base + "count.txt.a").should.exist
-            (base + "count.txt.a").size.should > 0
-            (base + "count.txt.b").should.exist
-            (base + "count.txt.b").size.should > 0
-            (base + "count.txt.c").should.exist
-            (base + "count.txt.c").size.should > 0
-            (base + "count.txt.d").should.exist
-            (base + "count.txt.d").size.should > 0
+            (base + "output" + "count.txt.a").should.exist
+            (base + "output" + "count.txt.a").size.should > 0
+            (base + "output" + "count.txt.b").should.exist
+            (base + "output" + "count.txt.b").size.should > 0
+            (base + "output" + "count.txt.c").should.exist
+            (base + "output" + "count.txt.c").size.should > 0
+            (base + "output" + "count.txt.d").should.exist
+            (base + "output" + "count.txt.d").size.should > 0
           end
         end
       else
@@ -125,16 +125,16 @@ TestHelper.scope do |this|
         runner.title = "should get a result of example/OddSelector"
         runner.args = ["example/OddSelector", "--rehearse", *runner.default_arguments]
         runner.run do |base|
-          (base + "1.res").should.exist
-          (base + "2.res").should.not.exist
-          (base + "3.res").should.exist
-          (base + "4.res").should.not.exist
-          (base + "5.res").should.exist
-          (base + "6.res").should.not.exist
-          (base + "7.res").should.exist
-          (base + "8.res").should.not.exist
-          (base + "9.res").should.exist
-          (base + "10.res").should.not.exist
+          (base + "output" + "1.res").should.exist
+          (base + "output" + "2.res").should.not.exist
+          (base + "output" + "3.res").should.exist
+          (base + "output" + "4.res").should.not.exist
+          (base + "output" + "5.res").should.exist
+          (base + "output" + "6.res").should.not.exist
+          (base + "output" + "7.res").should.exist
+          (base + "output" + "8.res").should.not.exist
+          (base + "output" + "9.res").should.exist
+          (base + "output" + "10.res").should.not.exist
         end
       end
     end
@@ -144,21 +144,21 @@ TestHelper.scope do |this|
         runner.title = "should get a result of example/SerialProcessing"
         runner.args = ["example/SerialProcessing", "--rehearse", *runner.default_arguments]
         runner.run do |base|
-          (base + "1.a").should.exist
-          (base + "2.a").should.exist
-          (base + "3.a").should.exist
-          (base + "4.a").should.exist
-          (base + "1.b").should.exist
-          (base + "2.b").should.exist
-          (base + "3.b").should.exist
-          (base + "4.b").should.exist
-          (base + "1.a").mtime.should <= (base + "2.a").mtime
-          (base + "2.a").mtime.should <= (base + "3.a").mtime
-          (base + "3.a").mtime.should <= (base + "4.a").mtime
-          (base + "4.a").mtime.should <= (base + "1.b").mtime
-          (base + "1.b").mtime.should <= (base + "2.b").mtime
-          (base + "2.b").mtime.should <= (base + "3.b").mtime
-          (base + "3.b").mtime.should <= (base + "4.b").mtime
+          (base + "output" + "1.a").should.exist
+          (base + "output" + "2.a").should.exist
+          (base + "output" + "3.a").should.exist
+          (base + "output" + "4.a").should.exist
+          (base + "output" + "1.b").should.exist
+          (base + "output" + "2.b").should.exist
+          (base + "output" + "3.b").should.exist
+          (base + "output" + "4.b").should.exist
+          (base + "output" + "1.a").mtime.should <= (base + "output" + "2.a").mtime
+          (base + "output" + "2.a").mtime.should <= (base + "output" + "3.a").mtime
+          (base + "output" + "3.a").mtime.should <= (base + "output" + "4.a").mtime
+          (base + "output" + "4.a").mtime.should <= (base + "output" + "1.b").mtime
+          (base + "output" + "1.b").mtime.should <= (base + "output" + "2.b").mtime
+          (base + "output" + "2.b").mtime.should <= (base + "output" + "3.b").mtime
+          (base + "output" + "3.b").mtime.should <= (base + "output" + "4.b").mtime
         end
       end
     end
@@ -170,11 +170,11 @@ TestHelper.scope do |this|
         runner.run do |base|
           1.upto(5) do |i|
             1.upto(5) do |ii|
-              comb = (base + "comb-%s-%s.pair" % [i, ii])
+              comb = (base + "output" + "comb-%s-%s.pair" % [i, ii])
               i < ii ? comb.should.exist : comb.should.not.exist
-              perm = (base + "perm-%s-%s.pair" % [i, ii])
+              perm = (base + "output" + "perm-%s-%s.pair" % [i, ii])
               i != ii ? perm.should.exist : perm.should.not.exist
-              succ = (base + "succ-%s-%s.pair" % [i, ii])
+              succ = (base + "output" + "succ-%s-%s.pair" % [i, ii])
               ii - i == 1 ? succ.should.exist : succ.should.not.exist
             end
           end
@@ -187,8 +187,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result of scenario a of example/SelectRuleByParam"
         runner.args = ["example/SelectRuleByParam", "--rehearse", "Select A", *runner.default_arguments]
         runner.run do |base|
-          (base + "message.txt").should.exist
-          (base + "message.txt").read.chomp.should == "This is rule A."
+          (base + "output" + "message.txt").should.exist
+          (base + "output" + "message.txt").read.chomp.should == "This is rule A."
         end
       end
 
@@ -196,8 +196,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result of scenario b of example/SelectRuleByParam"
         runner.args = ["example/SelectRuleByParam", "--rehearse", "Select B", *runner.default_arguments]
         runner.run do |base|
-          (base + "message.txt").should.exist
-          (base + "message.txt").read.chomp.should == "This is rule B."
+          (base + "output" + "message.txt").should.exist
+          (base + "output" + "message.txt").read.chomp.should == "This is rule B."
         end
       end
 
@@ -205,8 +205,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result of scenario c of example/SelectRuleByParam"
         runner.args = ["example/SelectRuleByParam", "--rehearse", "Select C", *runner.default_arguments]
         runner.run do |base|
-          (base + "message.txt").should.exist
-          (base + "message.txt").read.chomp.should == "This is rule C."
+          (base + "output" + "message.txt").should.exist
+          (base + "output" + "message.txt").read.chomp.should == "This is rule C."
         end
       end
     end
@@ -216,8 +216,8 @@ TestHelper.scope do |this|
         runner.title = "should get a result of example/FeatureExample"
         runner.args = ["example/FeatureExample", "--feature", "^X", *runner.default_arguments]
         runner.run do |base|
-          (base + "message.txt").should.exist
-          (base + "message.txt").read.chomp.should == "I can take the task."
+          (base + "output" + "message.txt").should.exist
+          (base + "output" + "message.txt").read.chomp.should == "I can take the task."
         end
       end
 
@@ -233,7 +233,7 @@ TestHelper.scope do |this|
         runner.title = "should get a result of example/Sum"
         runner.args = ["example/Sum", "--rehearse", *runner.default_arguments]
         runner.run do |base|
-          (base + "sum.txt").should.exist
+          (base + "output" + "sum.txt").should.exist
         end
       end
     end
